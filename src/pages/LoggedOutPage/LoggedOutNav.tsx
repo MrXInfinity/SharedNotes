146a455595a32dda3e3ae1cf6347f1caa116dfae
Johann Mendoza <johannmendoza1209@gmail.com>
2023-04-01 15:59:53 +0800

    css changes for loggedout nav

--- src/pages/LoggedOutPage/LoggedOutNav.tsx
+++ src/pages/LoggedOutPage/LoggedOutNav.tsx
@@ -1,98 +1,101 @@
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 import {
   Box,
   Container,
   IconButton,
-  Menu,
-  MenuItem,
   Stack,
   Typography,
   useTheme,
 } from "@mui/material";
-import DarkModeIcon from "@mui/icons-material/DarkMode";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
-import LinkIcon from "@mui/icons-material/Link";
-import notepadBg from "../../assets/notepad-bg.svg";
-import React, { useState } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
+import LoggedOutBg from "../../assets/logoutBG.jpg";
+
+const LinkIcon: React.FC<{ link?: string; icon: any; click?: () => void }> = ({
+  link,
+  icon,
+  click,
+}) => {
+  const theme = useTheme();
+  return (
+    <IconButton onClick={click ? () => click() : undefined}>
+      <a
+        style={{ color: theme.palette.primary.main }}
+        href={link}
+      >
+        {icon}
+      </a>
+    </IconButton>
+  );
+};
 
 const LoggedOutNav: React.FC<{ changeTheme: () => void }> = ({
   changeTheme,
 }) => {
   const theme = useTheme();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const showMenu = Boolean(anchorEl);
-
-  const LinkMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const MenuClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <Container
       style={{
+        background: `url("${LoggedOutBg}")`,
+        backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-        background: `url("${notepadBg}") no-repeat center`,
+        backgroundPosition: "center",
+      }}
+      maxWidth="xl"
+      disableGutters
+      sx={{
+        px: 0,
         height: "100vh",
       }}
-      sx={{ px: 0 }}
     >
       <Box
         sx={{
+          backgroundColor: "background.paper",
           display: "flex",
           alignItems: "center",
           justifyContent: "space-between",
+          py: { xs: 1, sm: 2, md: 2 },
           px: 3,
-          py: 4,
+          opacity: 0.9,
         }}
       >
-        <Typography variant="h5">ShareNotes</Typography>
+        <Typography
+          sx={{ fontWeight: 450, fontSize: { xs: 16, md: 18 } }}
+          color="primary"
+        >
+          ShareNotes
+        </Typography>
         <Stack
           direction="row"
           spacing={{ xs: 1, sm: 2 }}
         >
-          <IconButton
-            onClick={() => changeTheme()}
-            color="primary"
-          >
-            {theme.palette.mode === "dark" ? (
-              <Brightness7Icon />
-            ) : (
-              <DarkModeIcon />
-            )}
-          </IconButton>
-          <div>
-            <IconButton
-              color="primary"
-              onClick={LinkMenuClick}
-              aria-controls={showMenu ? "basic-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={showMenu ? "true" : undefined}
-            >
-              <LinkIcon />
-            </IconButton>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={showMenu}
-              onClose={MenuClose}
-              MenuListProps={{
-                "aria-labelledby": "basic-button",
-              }}
-            >
-              <MenuItem>
-                <a href="https://www.facebook.com">Facebook</a>
-              </MenuItem>
-              <MenuItem>
-                <a href="https://www.twitter.com">Twitter</a>
-              </MenuItem>
-              <MenuItem>
-                <a href="https://www.youtube.com">Youtube</a>
-              </MenuItem>
-            </Menu>
-          </div>
+          <LinkIcon
+            icon={
+              theme.palette.mode === "dark" ? (
+                <Brightness7Icon />
+              ) : (
+                <DarkModeIcon />
+              )
+            }
+            click={() => changeTheme()}
+          />
+          <LinkIcon
+            link="https://www.facebook.com"
+            icon={<FacebookIcon />}
+          />
+          <LinkIcon
+            link="https://www.github.com"
+            icon={<GitHubIcon />}
+          />
+          <LinkIcon
+            link="https://www.youtube.com"
+            icon={<YouTubeIcon />}
+          />
         </Stack>
       </Box>
       <Outlet />
